8916128532957f2e8b1a510f67738973aa0a1169
Kristine Fernandez <kfernandez@KristinandezMac.lan>
2023-05-28 16:30:35 -0500

    exclude back to on iframe

--- shared/blogroll.js
+++ shared/blogroll.js
@@ -1,10 +1,19 @@
-const anchorTag = document.createElement("a");  
-anchorTag.href = "https://www.kristinefernandez.com/blog";
-anchorTag.target = "_blank";
-anchorTag.innerText = "Back to kristinefernandez.com";
-anchorTag.classList.add("blogroll");
-const link = document.createElement("link");
-link.rel = "stylesheet";
-link.href = "../shared/blogroll.css";
-document.getElementsByTagName("head")[0].appendChild(link);
-document.getElementsByTagName("body")[0].appendChild(anchorTag);
+// get query string
+const queryString = window.location.search;
+// get url params
+const urlParams = new URLSearchParams(queryString);
+// get blogroll param
+const iframe = urlParams.get('iframe');
+
+if (!iframe){
+    const anchorTag = document.createElement("a");  
+    anchorTag.href = "https://www.kristinefernandez.com/blog";
+    anchorTag.target = "_blank";
+    anchorTag.innerText = "Back to kristinefernandez.com";
+    anchorTag.classList.add("blogroll");
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "../shared/blogroll.css";
+    document.getElementsByTagName("head")[0].appendChild(link);
+    document.getElementsByTagName("body")[0].appendChild(anchorTag);
+}
