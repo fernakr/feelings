83b4704f7d263c00a0b1a30e97d881bcf10850f9
Kristine Fernandez <kfernandez@KristinandezMac.lan>
2023-08-19 19:42:15 -0500

    vulnerability game

---
+++ vulnerability/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const pkg = require('./package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const buildPath = './build/';
+
+module.exports = {
+  entry: ['./app.js'],
+  output: {
+    path: path.join(__dirname, buildPath),
+    filename: '[name].[hash].js'
+  },
+  mode: 'development',
+  target: 'web',
+  devtool: 'source-map',
+  // resolve: {
+  //   extensions: ['.tsx', '.ts', '.js']
+  // },
+  node: {
+    module: 'empty',
+    fs: 'empty'
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: path.resolve(__dirname, './node_modules/')
+      },{
+        test: /\.(jpe?g|png|gif|svg|tga|glb|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg|fbx)$/i,
+        use: 'file-loader',
+        exclude: path.resolve(__dirname, './node_modules/')
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({'title': 'three-seed project'})
+  ]
+}
